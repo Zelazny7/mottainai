edfd1a3ca84c11bafe1d22e9aa956696faed38d0
Zelazny7 <gravcon5@gmail.com>
2019-08-22 08:35:26 -0500

    added player class

--- mottainai/__init__.py
+++ mottainai/__init__.py
@@ -1,2 +1,3 @@
 from player import Player
 from game import Game
+from card import Card
--- mottainai/player.py
+++ mottainai/player.py
@@ -1,2 +1,15 @@
-class Player():
-    pass
+from typing import List, Optional
+from mottaini import Card
+
+
+class Player:
+    """Class representing the state of a player's board"""
+
+    def __init__(self) -> None:
+        self.craft_bench: List[Card] = []
+        self.helpers: List[Card] = []
+        self.sales: List[Card] = []
+        self.gallery: List[Card] = []
+        self.gift_shop: List[Card] = []
+        self.task: Optional[Card] = None
+
